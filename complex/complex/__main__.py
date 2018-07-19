db3c6a1a7ec5841cf1e643704599ae1713d52be1
Matt Patrick <hazmat345@gmail.com>
2018-07-19 13:35:23 -0400

    Using get_argument_parser to grab command line arguments

--- complex/complex/__main__.py
+++ complex/complex/__main__.py
@@ -2,25 +2,26 @@ from __future__ import absolute_import
 
 import sys
 
-from brewtils import get_connection_info, Plugin
+from brewtils import get_argument_parser, get_connection_info, Plugin
 from .client import ComplexClient
-from .errors import StartupError
 
 __version__ = '1.0.0.dev0'
 
 
 def main():
-    if len(sys.argv) < 4:
-        raise StartupError("%d arguments provided (3 required:'instance', "
-                           "'host' and 'port')" % (len(sys.argv)-1))
+    parser = get_argument_parser()
+    parser.add_argument('instance_name')
+    parser.add_argument('host')
+    parser.add_argument('port')
+    config = parser.parse_args(sys.argv[1:])
 
     Plugin(
-        ComplexClient(sys.argv[2], sys.argv[3]),
+        ComplexClient(config.host, config.port),
         name='complex',
         version=__version__,
-        instance_name=sys.argv[1],
+        instance_name=config.instance_name,
         max_instances=2,
-        **get_connection_info(sys.argv[4:])
+        **get_connection_info(cli_args=sys.argv[1:], argument_parser=parser)
     ).run()
 
 
