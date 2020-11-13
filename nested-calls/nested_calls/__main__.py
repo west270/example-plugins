7c54cd831026376bbeb416b62285518052b4c7d0
Matt Patrick <hazmat345@gmail.com>
2020-11-13 17:42:21 -0500

    Fixing nested calls commands

--- nested-calls/nested_calls/__main__.py
+++ nested-calls/nested_calls/__main__.py
@@ -7,34 +7,32 @@ __version__ = "3.0.0.dev0"
 class NestedCallClient:
     """A client that calls nested commands as System Clients"""
 
-    nestedCallsClient = None
-
-    def post_init(self):
+    def __init__(self):
         self.nestedCallsClient = SystemClient(system_name="nested-calls")
 
     @command(description="Start Commands at Level One")
     def level_1(self):
         """Starts Commands at Level One"""
 
-        return self.nestedCallsClient.level_2()
+        return self.nestedCallsClient.level_2().output
 
     @command(description="Start Commands at Level Two")
     def level_2(self):
         """Starts Commands at Level One"""
 
-        return self.nestedCallsClient.level_3()
+        return self.nestedCallsClient.level_3().output
 
     @command(description="Start Commands at Level Three")
     def level_3(self):
         """Starts Commands at Level Three"""
 
-        return self.nestedCallsClient.level_4()
+        return self.nestedCallsClient.level_4().output
 
     @command(description="Start Commands at Level Four")
     def level_4(self):
         """Starts Commands at Level Four"""
 
-        return self.nestedCallsClient.level_5()
+        return self.nestedCallsClient.level_5().output
 
     @command(description="Start Commands at Level Five")
     def level_5(self):
@@ -47,10 +45,9 @@ def main():
     plugin = Plugin(
         name="nested-calls",
         version=__version__,
-        description="A plugin that's calls commands in itself as System Clients",
+        description="A plugin that calls commands on itself",
     )
     plugin.client = NestedCallClient()
-    plugin.client.post_init()
     plugin.run()
 
 
